148a2c2262866b893906b4c9be90f3874885af38
mikolalysenko <mikolalysenko@gmail.com>
2017-09-21 15:43:32 +0800

    add default exports for helnet and helschema

---
+++ helschema/src/index.ts
@@ -0,0 +1,37 @@
+import HelModel from './model';
+
+import HelInt8 from './int8';
+import HelInt16 from './int16';
+import HelInt32 from './int32';
+
+import HelUint8 from './uint8';
+import HelUint16 from './uint16';
+import HelUint32 from './uint32';
+
+import HelFloat32 from './float32';
+import HelFloat64 from './float64';
+
+import HelString from './string';
+
+import HelBoolean from './boolean';
+
+import HelStruct from './struct';
+import HelUnion from './union';
+
+import HelDictionary from './dictionary';
+
+export default {
+    HelInt8,
+    HelInt16,
+    HelInt32,
+    HelUint8,
+    HelUint16,
+    HelUint32,
+    HelFloat32,
+    HelFloat64,
+    HelString,
+    HelBoolean,
+    HelStruct,
+    HelUnion,
+    HelDictionary
+};
