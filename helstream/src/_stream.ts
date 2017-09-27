c7df963ce4a9cb39160d504993dbd13541cb6da5
mikolalysenko <mikolalysenko@gmail.com>
2017-09-27 13:59:55 +0800

    refactor stream apis

--- helstream/src/_stream.ts
+++ helstream/src/_stream.ts
@@ -24,25 +24,17 @@ function ceilLog2 (v_) {
 export class HelBuffer {
     public buffer:ArrayBuffer;
     public uint8:Uint8Array;
-    public uint16:Uint16Array[];
-    public uint32:Uint32Array[];
+    public uint16:Uint16Array;
+    public uint32:Uint32Array;
 
     // TODO finish support for all types
 
-    constructor (size:number) {
-        const uint8 = this.uint8 = new Uint8Array(size);
-        const buffer = this.buffer = uint8.buffer;
+    constructor (buffer:ArrayBuffer) {
+        this.buffer = buffer;
 
-        this.uint16 = [
-            new Uint16Array(buffer), 
-            new Uint16Array(buffer, 1)
-        ];
-        this.uint32 = [
-            new Uint32Array(buffer), 
-            new Uint32Array(buffer, 1), 
-            new Uint32Array(buffer, 2),
-            new Uint32Array(buffer, 3)
-        ];
+        this.uint8 = new Uint8Array(buffer);
+        this.uint16 = new Uint16Array(buffer);
+        this.uint32 = new Uint32Array(buffer);
     }
 };
 
@@ -54,7 +46,7 @@ for (let i = 0; i < 32; ++i) {
 
 export function allocBuffer (sz) : HelBuffer {
     const b = ceilLog2(sz);
-    return bufferPool[b].pop() || new HelBuffer(1 << b);
+    return bufferPool[b].pop() || new HelBuffer(new Uint8Array(1 << b).buffer);
 }
 
 export function freeBuffer (buffer:HelBuffer) {
@@ -72,7 +64,9 @@ export function reallocBuffer (buffer:HelBuffer, nsize:number) {
     return result;
 }
 
-export class HelStream {
+const SCRATCH_BUFFER = new HelBuffer(new Uint8Array(8).buffer);
+
+export class HelWriteStream {
     public buffer:HelBuffer;
     public offset:number;
 
@@ -94,13 +88,49 @@ export class HelStream {
     }
 
     public writeUint16 (x:number) {
-        this.buffer.uint16[this.offset & 1][this.offset >> 1] = x;
+        const offset = this.offset;
+        if (offset & 1) {
+            SCRATCH_BUFFER.uint16[0] = x;
+            const xbytes = SCRATCH_BUFFER.uint8;
+            const bytes = this.buffer.uint8;
+            bytes[offset] = xbytes[0];
+            bytes[offset + 1] = xbytes[1];
+        } else {
+            this.buffer.uint16[offset >> 1] = x;
+        }
         this.offset += 2;
     }
 
     public writeUint32 (x:number) {
-        this.buffer.uint32[this.offset & 3][this.offset >> 2] = x;
-        this.offset += 4;
+        const offset = this.offset;
+        if (offset & 3) {
+            SCRATCH_BUFFER.uint32[0] = x;
+            const xbytes = SCRATCH_BUFFER.uint8;
+            const bytes = this.buffer.uint8;
+            bytes[offset] = xbytes[0];
+            bytes[offset + 1] = xbytes[1];
+            bytes[offset + 2] = xbytes[2];
+            bytes[offset + 3] = xbytes[3];
+        } else {
+            this.buffer.uint32[offset >> 2] = x;
+        }
+    }
+
+    public writeString (str:string) {
+        const bytes = encodeString(str);
+        this.writeUint32(bytes.length);
+        this.buffer.uint8.set(bytes, this.offset);
+        this.offset += bytes.length;
+    }
+
+}
+
+export class HelReadStream {
+    public buffer:HelBuffer;
+    public offset:number = 0;
+
+    constructor (buffer:ArrayBuffer) {
+        this.buffer = new HelBuffer(buffer);
     }
 
     public readUint8 () : number {
@@ -110,20 +140,29 @@ export class HelStream {
     public readUint16 () : number {
         const offset = this.offset;
         this.offset += 2;
-        return this.buffer.uint16[offset & 1][offset >> 1];
+        if (offset & 1) {
+            const bytes = this.buffer.uint8;
+            const xbytes = SCRATCH_BUFFER.uint8;
+            xbytes[0] = bytes[offset];
+            xbytes[1] = bytes[offset + 1];
+            return SCRATCH_BUFFER.uint16[0];
+        }
+        return this.buffer.uint16[offset >> 1];                
     }
 
     public readUint32 () : number {
         const offset = this.offset;
         this.offset += 4;
-        return this.buffer.uint16[offset & 3][offset >> 2];
-    }
-
-    public writeString (str:string) {
-        const bytes = encodeString(str);
-        this.writeUint32(bytes.length);
-        this.buffer.uint8.set(bytes, this.offset);
-        this.offset += bytes.length;
+        if (offset & 3) {
+            const bytes = this.buffer.uint8;
+            const xbytes = SCRATCH_BUFFER.uint8;
+            xbytes[0] = bytes[offset];
+            xbytes[1] = bytes[offset + 1];
+            xbytes[2] = bytes[offset + 2];
+            xbytes[3] = bytes[offset + 3];
+            return SCRATCH_BUFFER.uint32[0];
+        }
+        return this.buffer.uint32[offset >> 2];
     }
 
     public readString () {
