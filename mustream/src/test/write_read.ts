ebc953cc49da0a1e9fed48ef885041a9f05bdbc0
esdoppio <esdoppio@gmail.com>
2017-10-20 11:42:23 +0800

    more tests

--- mustream/src/test/write_read.ts
+++ mustream/src/test/write_read.ts
@@ -2,30 +2,70 @@ import * as test from 'tape';
 import { MuWriteStream, MuReadStream } from '../';
 import { reallocBuffer, allocBuffer, freeBuffer } from '../';
 
+test('buffer allocation', (t) => {
+  t.equals(allocBuffer(8).buffer.byteLength, 8);
+  t.equals(allocBuffer(9).buffer.byteLength, 16);
+  t.equals(allocBuffer(15).buffer.byteLength, 16);
+
+  t.end();
+});
+
 test('int', (t) => {
-  const ws = new MuWriteStream(16);
-  ws.writeUint16(1024);
-  ws.writeUint16(144);
-  ws.writeUint8(128);
+  let ws = new MuWriteStream(8);
+  ws.writeUint32(1234567890);
+  ws.writeUint16(12345);
+  ws.writeUint8(123);
 
-  const rs = new MuReadStream(ws.buffer.buffer);
+  let rs = new MuReadStream(ws.buffer.buffer);
 
-  t.equal(rs.readUint16(), 1024);
-  t.equal(rs.readUint16(), 144);
-  t.equal(rs.readUint8(), 128);
+  t.equals(rs.readUint32(), 1234567890);
+  t.equals(rs.readUint16(), 12345);
+  t.equals(rs.readUint8(), 123);
+
+  ws = new MuWriteStream(8);
+  ws.writeUint8(255);
+  ws.writeUint16(65535);
+  ws.writeUint32(4294967295);
+
+  rs = new MuReadStream(ws.buffer.buffer);
+
+  t.equals(rs.readUint32(), 4294967295);
+  t.equals(rs.readUint16(), 65535);
+  t.equals(rs.readUint8(), 255);
+
+  ws = new MuWriteStream(8);
+  ws.writeUint8(256);
+  ws.writeUint16(65536);
+  ws.writeUint32(4294967296);
+
+  rs = new MuReadStream(ws.buffer.buffer);
+
+  t.equals(rs.readUint8(), 0);
+  t.equals(rs.readUint16(), 0);
+  t.equals(rs.readUint32(), 0);
 
   t.end();
 });
 
 test('float64', (t) => {
-  const ws = new MuWriteStream(16);
+  let ws = new MuWriteStream(16);
+  ws.writeFloat64(1024.256);
+  ws.writeUint16(2018);
+
+  let rs = new MuReadStream(ws.buffer.buffer);
+
+  t.equals(rs.readFloat64(), 1024.256);
+  t.equals(rs.readUint16(), 2018);
+
+  ws = new MuWriteStream(16);
+
   ws.writeUint16(2018);
   ws.writeFloat64(1024.256);
 
-  const rs = new MuReadStream(ws.buffer.buffer);
+  rs = new MuReadStream(ws.buffer.buffer);
 
-  t.equal(rs.readUint16(), 2018);
-  t.equal(rs.readFloat64(), 1024.256);
+  t.equals(rs.readUint16(), 2018);
+  t.equals(rs.readFloat64(), 1024.256);
 
   t.end();
 });
