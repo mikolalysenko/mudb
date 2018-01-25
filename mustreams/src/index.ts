2de2d11b72eca03b8c38c0842b23b10a489c2f52
mikolalysenko <mikolalysenko@gmail.com>
2018-01-25 14:26:01 +0800

    add varint support to mustreams

--- mustreams/src/index.ts
+++ mustreams/src/index.ts
@@ -120,6 +120,38 @@ export class MuWriteStream {
         this.offset += 8;
     }
 
+    public writeVarInt (x:number) {
+        const bytes = this.buffer.uint8;
+        const offset = this.offset;
+
+        if (x < (1 << 7)) {
+            bytes[offset] = x;
+            this.offset += 1;
+        } else if (x < (1 << 14)) {
+            bytes[offset] = 0x80 | (x & 0x7f);
+            bytes[offset + 1] = x >> 7;
+            this.offset += 2;
+        } else if (x < (1 << 21)) {
+            bytes[offset] = 0x80 | (x & 0x7f);
+            bytes[offset + 1] = 0x80 | ((x >> 7) & 0x7f);
+            bytes[offset + 2] = x >> 14;
+            this.offset += 3;
+        } else if (x < (1 << 28)) {
+            bytes[offset] = 0x80 | (x & 0x7f);
+            bytes[offset + 1] = 0x80 | ((x >> 7) & 0x7f);
+            bytes[offset + 2] = 0x80 | ((x >> 14) & 0x7f);
+            bytes[offset + 3] = x >> 21;
+            this.offset += 4;
+        } else {
+            bytes[offset] = 0x80 | (x & 0x7f);
+            bytes[offset + 1] = 0x80 | ((x >> 7) & 0x7f);
+            bytes[offset + 2] = 0x80 | ((x >> 14) & 0x7f);
+            bytes[offset + 3] = 0x80 | ((x >> 21) & 0x7f);
+            bytes[offset + 4] = x >> 28;
+            this.offset += 5;
+        }
+    }
+
     public writeASCII (str:string) {
         for (let i = 0; i < str.length; ++i) {
             this.writeUint8(str.charCodeAt(i));
@@ -195,6 +227,44 @@ export class MuReadStream {
         const offset = this.offset;
         this.offset += 8;
         return this.buffer.dataView.getFloat64(offset, LITTLE_ENDIAN);
+    }
+
+    public readVarInt () : number {
+        const bytes = this.buffer.uint8;
+        let offset = this.offset;
+        const x0 = bytes[offset++];
+        if (x0 < 0x80) {
+            this.offset = offset;
+            return x0;
+        }
+        const x1 = bytes[offset++];
+        if (x1 < 0x80) {
+            this.offset = offset;
+            return (x0 & 0x7f) |
+                (x1 << 7);
+        }
+        const x2 = bytes[offset++]
+        if (x2 < 0x80) {
+            this.offset = offset;
+            return (x0 & 0x7f) |
+                ((x1 << 7) & 0x7f) |
+                (x2 << 14);
+        }
+        const x3 = bytes[offset++]
+        if (x3 < 0x80) {
+            this.offset = offset;
+            return (x0 & 0x7f) |
+                ((x1 << 7) & 0x7f) |
+                ((x2 << 14) & 0x7f) |
+                (x3 << 21);
+        }
+        const x4 = bytes[offset++]
+        this.offset = offset;
+        return (x0 & 0x7f) |
+            ((x1 << 7) & 0x7f) |
+            ((x2 << 14) & 0x7f) |
+            ((x3 << 21) & 0x7f) |
+            (x4 << 28);
     }
 
     public readASCII (byteLength:number) : string {
