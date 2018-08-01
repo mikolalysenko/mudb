c969bc8de40c35d602617b8b593049bfbbc3badd
He Diyi <esdoppio@gmail.com>
2018-08-01 14:53:01 +0800

    web-socket: do not run tests in watch mode by default

--- modules/muweb-socket/bin/test-server.js
+++ modules/muweb-socket/bin/test-server.js
@@ -1,13 +1,11 @@
 const path = require('path')
 const { spawn } = require('child_process')
-const { watch } = require('chokidar')
 
-const modulePath = path.resolve(__dirname, '..')
-watch(`${modulePath}/src`)
-    .on('ready', () => {
-        spawn('node', [ 'test/server.js' ], { stdio: 'inherit' })
-    })
-    .on('change', (file) => {
-        console.log(`${path.basename(file)} has been changed...\n`)
-        spawn('node', [ 'test/server.js' ], { stdio: 'inherit' })
-    })
+const moduleRoot = path.resolve(__dirname, '..')
+spawn(
+    'node', [ 'test/server.js' ],
+    {
+        cwd: moduleRoot,
+        stdio: 'inherit',
+    }
+)
--- modules/muweb-socket/bin/test-socket.js
+++ modules/muweb-socket/bin/test-socket.js
@@ -1,48 +1,27 @@
 const path = require('path')
-const { exec, spawn } = require('child_process')
-const getPort = require('get-port')
-const { watch } = require('chokidar')
+const { spawn } = require('child_process')
 
-let port = 0
+const getPort = require('get-port')
 
 getPort().then(
-    (port_) => {
-        port = port_
-        spawn(
-            'node', [ 'start-server.js', port_ ],
-            {
-                cwd: __dirname,
-                stdio: 'inherit',
-            },
+    (port) => {
+        const server = spawn(
+            'node', [ 'start-server.js', port ],
+            { cwd: __dirname },
         )
-    },
-    console.error,
-)
 
-const modulePath = path.resolve(__dirname, '..')
-watch(`${modulePath}/src`)
-    .on('ready', () => {
-        exec(
-            `browserify test/socket.js -t [ envify --PORT ${port} ] | tape-run`,
-            (error, stdout) => {
-                if (error) {
-                    console.error(error)
-                    return
-                }
-                console.log(stdout)
-            }
-        )
-    })
-    .on('change', (file) => {
-        console.log(`${path.basename(file)} has been changed...\n`)
-        exec(
-            `browserify test/socket.js -t [ envify --PORT ${port} ] | tape-run`,
-            (error, stdout) => {
-                if (error) {
-                    console.error(error)
-                    return
-                }
-                console.log(stdout)
-            }
-        )
-    })
+        server.stdout.on('data', (msg) => {
+            console.log(msg.toString())
+
+            const moduleRoot = path.resolve(__dirname, '..')
+            spawn(
+                `browserify test/socket.js -t [ envify --PORT ${port} ] | tape-run`,
+                {
+                    shell: true,
+                    cwd: moduleRoot,
+                    stdio: 'inherit',
+                },
+            )
+        })
+    },
+)
