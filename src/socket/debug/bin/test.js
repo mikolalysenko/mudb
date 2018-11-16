9ee70ad61284a51f94d77025065f35c7c306299e
He Diyi <esdoppio@gmail.com>
2018-11-16 11:52:50 +0800

    socket: stop using get-port in tests

--- src/socket/debug/bin/test.js
+++ src/socket/debug/bin/test.js
@@ -1,6 +1,17 @@
+const tcp = require('net')
 const path = require('path')
 const { spawnSync, spawn } = require('child_process')
-const getPort = require('get-port')
+
+function getFreePort (cb) {
+    const server = tcp.createServer()
+    server.on('error', (err) => console.error(err))
+    server.unref()
+
+    server.listen(() => {
+        const port = server.address().port
+        server.close(() => cb(port))
+    })
+}
 
 const modulePath = path.resolve(__dirname, '..')
 
@@ -12,25 +23,22 @@ spawnSync(
     },
 )
 
-getPort().then(
-    (port) => {
-        const server = spawn(
-            'node',
-            [ 'test/start-server.js', port ],
-            { cwd: modulePath },
-        )
+getFreePort((port) => {
+    const server = spawn(
+        'node',
+        [ 'test/start-server.js', port ],
+        { cwd: modulePath },
+    )
 
-        server.stdout.once('data', (output) => {
-            console.log(output.toString())
-            spawn(
-                `browserify test/web.js -t [ envify --PORT ${port} ] | tape-run`,
-                {
-                    cwd: modulePath,
-                    shell: true,
-                    stdio: 'inherit',
-                },
-            ).on('exit', process.exit)
-        })
-    },
-    console.error,
-)
+    server.stdout.once('data', (output) => {
+        console.log(output.toString())
+        spawn(
+            `browserify test/web.js -t [ envify --PORT ${port} ] | tape-run`,
+            {
+                cwd: modulePath,
+                shell: true,
+                stdio: 'inherit',
+            },
+        ).on('exit', process.exit)
+    })
+})
--- src/socket/web/bin/test.js
+++ src/socket/web/bin/test.js
@@ -1,42 +1,50 @@
+const tcp = require('net')
 const path = require('path')
 const { spawn, spawnSync } = require('child_process')
 
-const getPort = require('get-port')
+function getFreePort (cb) {
+    const server = tcp.createServer()
+    server.on('error', (err) => console.error(err))
+    server.unref()
+
+    server.listen(() => {
+        const port = server.address().port
+        server.close(() => cb(port))
+    })
+}
 
 const moduleRoot = path.resolve(__dirname, '..')
 
-getPort().then(
-    (port) => {
-        const server = spawn(
-            'node', [ 'start-server.js', port ],
-            { cwd: __dirname },
-        )
+getFreePort((port) => {
+    const server = spawn(
+        'node', [ 'start-server.js', port ],
+        { cwd: __dirname },
+    )
 
-        server.stdout.once('data', (msg) => {
-            console.log(msg.toString())
+    server.stdout.once('data', (msg) => {
+        console.log(msg.toString())
 
-            console.log('testing socket in browser...')
+        console.log('testing socket in browser...')
+        spawn(
+            `browserify test/socket.js -t [ envify --PORT ${port} ] | tape-run`,
+            {
+                shell: true,
+                cwd: moduleRoot,
+                stdio: 'inherit',
+            },
+        ).on('exit', () => {
+            console.log('testing socket in node...')
             spawn(
-                `browserify test/socket.js -t [ envify --PORT ${port} ] | tape-run`,
+                'node', [ 'test/socket.js' ],
                 {
-                    shell: true,
                     cwd: moduleRoot,
+                    env: Object.assign({}, process.env, { PORT: port }),
                     stdio: 'inherit',
                 },
-            ).on('exit', () => {
-                console.log('testing socket in node...')
-                spawn(
-                    'node', [ 'test/socket.js' ],
-                    {
-                        cwd: moduleRoot,
-                        env: Object.assign({}, process.env, { PORT: port }),
-                        stdio: 'inherit',
-                    },
-                ).on('exit', process.exit)
-            })
+            ).on('exit', process.exit)
         })
-    },
-)
+    })
+})
 
 console.log('testing server...')
 const serverTest = spawnSync(
