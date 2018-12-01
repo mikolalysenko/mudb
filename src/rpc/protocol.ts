38a374d066a70640c25ac8ac072dc655fdb6c813
He Diyi <esdoppio@gmail.com>
2018-12-02 01:36:55 +0800

    rpc: remove obsolete code

--- src/rpc/protocol.ts
+++ src/rpc/protocol.ts
@@ -113,90 +113,6 @@ export namespace RPC {
     };
 }
 
-export type MuAnySchema = MuSchema<any>;
-export type MuRPCSchema = [ MuAnySchema, MuAnySchema ];
-export interface MuRPCTable {
-    [method:string]:MuRPCSchema;
-}
-export interface MuRPCProtocolSchema {
-    client:MuRPCTable;
-    server:MuRPCTable;
-}
-
-export type MuRPCError = string;
-export interface MuRPCInterface<RPCTable extends MuRPCTable> {
-    callerAPI:{
-        [method in keyof RPCTable]:(
-            arg:RPCTable[method][0]['identity'],
-            callback?:(response:RPCTable[method][1]['identity']) => void,
-        ) => void
-    };
-    clientHandlerAPI:{
-        [method in keyof RPCTable]:(
-            arg:RPCTable[method][0]['identity'],
-            next:(err:MuRPCError|undefined, response?:RPCTable[method][1]['identity']) => void,
-        ) => void
-    };
-    serverHandlerAPI:{
-        [method in keyof RPCTable]:(
-            arg:RPCTable[method][0]['identity'],
-            next:(err:MuRPCError|undefined, response?:RPCTable[method][1]['identity']) => void,
-            client?:MuRPCRemoteClient<RPCTable>,
-        ) => void
-    };
-}
-
-export type MuRPCProtocolTablePhase<RPCTable extends MuRPCTable, Phase extends 0 | 1> = {
-    [method in keyof RPCTable]:MuStruct<{
-        base:RPCTable[method][Phase];
-        id:MuUint32;
-    }>;
-};
-
-export type MuRPCProtocolSchemaUnfolded<ProtocolSchema extends MuRPCProtocolSchema> = [
-    {
-        client:MuRPCProtocolTablePhase<ProtocolSchema['client'], 0>;
-        server:MuRPCProtocolTablePhase<ProtocolSchema['server'], 0>;
-    },
-    {
-        client:MuRPCProtocolTablePhase<ProtocolSchema['server'], 1>;
-        server:MuRPCProtocolTablePhase<ProtocolSchema['client'], 1>;
-    }
-];
-
-export function unfoldRPCProtocolSchema<ProtocolSchema extends MuRPCProtocolSchema> (
-    schema:ProtocolSchema,
-) : MuRPCProtocolSchemaUnfolded<ProtocolSchema> {
-    const result = [
-        { client: {}, server: {} },
-        { client: {}, server: {} },
-    ] as MuRPCProtocolSchemaUnfolded<ProtocolSchema>;
-    const CallbackIDSchema = new MuUint32();
-
-    Object.keys(schema.client).forEach((method) => {
-        result[0].client[method] = new MuStruct({
-            base: schema.client[method][0],
-            id: CallbackIDSchema,
-        });
-        result[1].server[method] = new MuStruct({
-            base: schema.client[method][1],
-            id: CallbackIDSchema,
-        });
-    });
-    Object.keys(schema.server).forEach((method) => {
-        result[0].server[method] = new MuStruct({
-            base: schema.server[method][0],
-            id: CallbackIDSchema,
-        });
-        result[1].client[method] = new MuStruct({
-            base: schema.server[method][1],
-            id: CallbackIDSchema,
-        });
-    });
-
-    return result;
-}
-
 // user-friendly tuple type assertion
 export function tuple<
     ArgumentSchema extends MuSchema<any>,
@@ -207,17 +123,3 @@ export function tuple<
 ) : [ArgumentSchema, ReturnSchema] {
     return [arg, ret];
 }
-
-export const MuRPCErrorSchema = new MuStruct({
-    message: new MuUTF8(),
-    id: new MuUint32(),
-});
-
-export const MuRPCErrorProtocolSchema = {
-    client: {
-        error: MuRPCErrorSchema,
-    },
-    server: {
-        error: MuRPCErrorSchema,
-    },
-};
