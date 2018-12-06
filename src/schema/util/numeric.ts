6d40d65f38b0a91b76b34a5cb1ed7622b413588f
He Diyi <esdoppio@gmail.com>
2018-12-06 16:54:50 +0800

    schema: inRange

--- src/schema/util/numeric.ts
+++ src/schema/util/numeric.ts
@@ -1,6 +1,22 @@
 export const fround = (function (a) {
-    return function (n:number) {
+    return function (n:number) : number {
         a[0] = n;
         return a[0];
     };
 })(new Float32Array(1));
+
+export const inRange = (function (rangeTable) {
+    return function (n:number, type:string) : boolean {
+        const range = rangeTable[type];
+        return n >= range[0] && n <= range[1];
+    };
+})({
+    float32:    [-3.4e+38,          3.4e+38],
+    float64:    [-Number.MAX_VALUE, Number.MAX_VALUE],
+    int8:       [-0x80,             0x7F],
+    int16:      [-0x8000,           0x7FFF],
+    int32:      [-0x80000000,       0x7FFFFFFF],
+    uint8:      [0,                 0xFF],
+    uint16:     [0,                 0xFFFF],
+    uint32:     [0,                 0xFFFFFFFF],
+});
