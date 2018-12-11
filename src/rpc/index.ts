48c4daf7463f2639364cac3cd56a4c72a45e95e9
He Diyi <esdoppio@gmail.com>
2018-12-11 17:55:42 +0800

    rpc: remove `tuple` from entry

--- src/rpc/index.ts
+++ src/rpc/index.ts
@@ -1,9 +1,7 @@
 import { MuRPCServer } from './server';
 import { MuRPCClient } from './client';
-import { tuple } from './protocol';
 
 export = {
     MuRPCServer,
     MuRPCClient,
-    tuple,
 };
