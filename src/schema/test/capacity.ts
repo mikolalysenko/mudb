d9dbf1a692920abfda5647598916521b140c665d
He Diyi <esdoppio@gmail.com>
2019-01-11 15:09:26 +0800

    should also guard patch() with capacity

--- src/schema/test/capacity.ts
+++ src/schema/test/capacity.ts
@@ -1,5 +1,5 @@
 import test = require('tape');
-import { MuWriteStream } from '../../stream';
+import { MuWriteStream, MuReadStream } from '../../stream';
 import {
     MuFloat32,
     MuArray,
@@ -7,7 +7,7 @@ import {
     MuDictionary,
 } from '../index';
 
-test('array.capacity', (t) => {
+test('guarding array.diff()', (t) => {
     let array = new MuArray(new MuFloat32(), Infinity);
     t.equal(array.capacity, Infinity);
     array = new MuArray(new MuFloat32(), 0);
@@ -20,12 +20,27 @@ test('array.capacity', (t) => {
     t.doesNotThrow(() => array.diff([], [0, 1], out));
     t.doesNotThrow(() => array.diff([0, 1, 2], [1, 2, 3], out));
     t.doesNotThrow(() => array.diff([0, 1, 2, 3], [1, 2, 3], out));
-    t.throws(() => array.diff([], [0, 1, 2, 3], out), RangeError, 'should throw when target size exceeds capacity');
+    t.throws(() => array.diff([], [0, 1, 2, 3], out), RangeError);
     t.throws(() => array.diff([], [0, 1, 2, 3, 4], out), RangeError);
     t.end();
 });
 
-test('sortedArray.capacity', (t) => {
+test('guarding array.patch()', (t) => {
+    const infiniteArray = new MuArray(new MuFloat32(), Infinity);
+    const out = new MuWriteStream(1);
+    infiniteArray.diff([], [1], out);
+    infiniteArray.diff([], [1, 2, 3], out);
+    infiniteArray.diff([], [1, 2, 3, 4], out);
+
+    const finiteArray = new MuArray(new MuFloat32(), 3);
+    const inp = new MuReadStream(out.buffer.uint8);
+    t.doesNotThrow(() => finiteArray.patch([], inp));
+    t.doesNotThrow(() => finiteArray.patch([], inp));
+    t.throws(() => finiteArray.patch([], inp), RangeError);
+    t.end();
+});
+
+test('guarding sortedArray.diff()', (t) => {
     let sortedArray = new MuSortedArray(new MuFloat32(), Infinity, (a, b) => a - b);
     t.equal(sortedArray.capacity, Infinity);
     sortedArray = new MuSortedArray(new MuFloat32(), 0, (a, b) => a - b);
@@ -43,7 +58,22 @@ test('sortedArray.capacity', (t) => {
     t.end();
 });
 
-test('dictionary.capacity', (t) => {
+test('guarding sortedArray.patch()', (t) => {
+    const infiniteSortedArray = new MuSortedArray(new MuFloat32(), Infinity);
+    const out = new MuWriteStream(1);
+    infiniteSortedArray.diff([], [1], out);
+    infiniteSortedArray.diff([], [1, 2, 3], out);
+    infiniteSortedArray.diff([], [1, 2, 3, 4], out);
+
+    const finiteSortedArray = new MuSortedArray(new MuFloat32(), 3);
+    const inp = new MuReadStream(out.buffer.uint8);
+    t.doesNotThrow(() => finiteSortedArray.patch([], inp));
+    t.doesNotThrow(() => finiteSortedArray.patch([], inp));
+    t.throws(() => finiteSortedArray.patch([], inp), RangeError);
+    t.end();
+});
+
+test('guarding dictionary.diff()', (t) => {
     let dictionary = new MuDictionary(new MuFloat32(), Infinity);
     t.equal(dictionary.capacity, Infinity);
     dictionary = new MuDictionary(new MuFloat32(), 0);
@@ -59,5 +89,20 @@ test('dictionary.capacity', (t) => {
     t.doesNotThrow(() => dictionary.diff({a: 0, b: 0, c: 0, d: 0}, {a: 0, b: 1, c: 2}, out));
     t.throws(() => dictionary.diff({}, {a: 0, b: 0, c: 0, d: 0}, out), RangeError, 'should throw when target size exceeds capacity');
     t.throws(() => dictionary.diff({}, {a: 0, b: 0, c: 0, d: 0, e: 0}, out), RangeError);
+    t.end();
+});
+
+test('guarding dictionary.patch()', (t) => {
+    const infiniteDictionary = new MuDictionary(new MuFloat32(), Infinity);
+    const out = new MuWriteStream(1);
+    infiniteDictionary.diff({}, {a: 0}, out);
+    infiniteDictionary.diff({}, {a: 0, b: 0, c: 0}, out);
+    infiniteDictionary.diff({}, {a: 0, b: 0, c: 0, d: 0}, out);
+
+    const finiteDictionary = new MuDictionary(new MuFloat32(), 3);
+    const inp = new MuReadStream(out.buffer.uint8);
+    t.doesNotThrow(() => finiteDictionary.patch({}, inp));
+    t.doesNotThrow(() => finiteDictionary.patch({}, inp));
+    t.throws(() => finiteDictionary.patch({}, inp), RangeError);
     t.end();
 });
