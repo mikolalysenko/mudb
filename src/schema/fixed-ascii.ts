287d9365380b59590f70ae805bf1d1eefba913de
He Diyi <esdoppio@gmail.com>
2019-01-25 13:13:57 +0800

    simplify fixed-ascii constructor

--- src/schema/fixed-ascii.ts
+++ src/schema/fixed-ascii.ts
@@ -5,15 +5,12 @@ export class MuFixedASCII extends MuString<'fixed-ascii'> {
     public readonly length:number;
 
     constructor (lengthOrIdentity:number|string) {
-        super(
-            typeof lengthOrIdentity === 'number' ?
-                Array(lengthOrIdentity + 1).join(' ') :
-                lengthOrIdentity,
-            'fixed-ascii',
-        );
-        this.length = typeof lengthOrIdentity === 'number' ?
-            lengthOrIdentity :
-            this.identity.length;
+        const identity = typeof lengthOrIdentity === 'number' ?
+            new Array(lengthOrIdentity + 1).join(' ') :
+            lengthOrIdentity;
+
+        super(identity, 'fixed-ascii');
+        this.length = identity.length;
     }
 
     public diff (base:string, target:string, out:MuWriteStream) : boolean {
