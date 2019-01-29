8d35d20a5cd48fc16a52a4dc57ab7d75d5ccc855
He Diyi <esdoppio@gmail.com>
2019-01-29 12:08:47 +0800

    only check size against capacity when patching

--- src/schema/test/capacity.ts
+++ src/schema/test/capacity.ts
@@ -7,24 +7,6 @@ import {
     MuDictionary,
 } from '../index';
 
-test('guarding array.diff()', (t) => {
-    let array = new MuArray(new MuFloat32(), Infinity);
-    t.equal(array.capacity, Infinity);
-    array = new MuArray(new MuFloat32(), 0);
-    t.equal(array.capacity, 0);
-    array = new MuArray(new MuFloat32(), 3);
-    t.equal(array.capacity, 3);
-
-    const out = new MuWriteStream(1);
-    t.doesNotThrow(() => array.diff([], [0], out));
-    t.doesNotThrow(() => array.diff([], [0, 1], out));
-    t.doesNotThrow(() => array.diff([0, 1, 2], [1, 2, 3], out));
-    t.doesNotThrow(() => array.diff([0, 1, 2, 3], [1, 2, 3], out));
-    t.throws(() => array.diff([], [0, 1, 2, 3], out), RangeError);
-    t.throws(() => array.diff([], [0, 1, 2, 3, 4], out), RangeError);
-    t.end();
-});
-
 test('guarding array.patch()', (t) => {
     const infiniteArray = new MuArray(new MuFloat32(), Infinity);
     const out = new MuWriteStream(1);
@@ -37,24 +19,6 @@ test('guarding array.patch()', (t) => {
     t.doesNotThrow(() => finiteArray.patch([], inp));
     t.doesNotThrow(() => finiteArray.patch([], inp));
     t.throws(() => finiteArray.patch([], inp), RangeError);
-    t.end();
-});
-
-test('guarding sortedArray.diff()', (t) => {
-    let sortedArray = new MuSortedArray(new MuFloat32(), Infinity, (a, b) => a - b);
-    t.equal(sortedArray.capacity, Infinity);
-    sortedArray = new MuSortedArray(new MuFloat32(), 0, (a, b) => a - b);
-    t.equal(sortedArray.capacity, 0);
-    sortedArray = new MuSortedArray(new MuFloat32(), 3, (a, b) => a - b);
-    t.equal(sortedArray.capacity, 3);
-
-    const out = new MuWriteStream(1);
-    t.doesNotThrow(() => sortedArray.diff([], [0], out));
-    t.doesNotThrow(() => sortedArray.diff([], [0, 1], out));
-    t.doesNotThrow(() => sortedArray.diff([0, 1, 2], [1, 2, 3], out));
-    t.doesNotThrow(() => sortedArray.diff([0, 1, 2, 3], [1, 2, 3], out));
-    t.throws(() => sortedArray.diff([], [0, 1, 2, 3], out), RangeError, 'should throw when target size exceeds capacity');
-    t.throws(() => sortedArray.diff([], [0, 1, 2, 3, 4], out), RangeError);
     t.end();
 });
 
