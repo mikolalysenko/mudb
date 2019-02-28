93243bd158cd7006b9d2ba5085966ff2793ae5f5
He Diyi <esdoppio@gmail.com>
2019-02-28 10:19:04 +0800

    more apply tests

--- src/rda/test/apply.ts
+++ src/rda/test/apply.ts
@@ -4,35 +4,26 @@ import { MuFloat64, MuUint8, MuStruct, MuUint32, MuUTF8, MuASCII, MuInt8 } from 
 import { MuRDAConstant, MuRDARegister, MuRDAStruct, MuRDAMap, MuRDAList } from '../index';
 
 test('constant apply', (t) => {
-    const FloatConstStore = new MuRDAConstant(new MuFloat64()).createStore(0);
-    t.false(FloatConstStore.apply(), 'should always be false');
-
-    const InitStatsStore = new MuRDAConstant(new MuStruct({
-        health: new MuUint8(),
-        mana: new MuUint8(),
-    })).createStore({
-        health: 100,
-        mana: 200,
-    });
-    t.false(InitStatsStore.apply(), 'should always be false');
+    const store = new MuRDAConstant(new MuFloat64()).createStore(0);
+    t.false(store.apply(), 'always false');
     t.end();
 });
 
 test('register apply', (t) => {
-    const FloatReg = new MuRDARegister(new MuFloat64());
-    const FloatRegStore = FloatReg.createStore(0);
-    t.true(FloatRegStore.apply(FloatReg, 123.123), 'should always be true');
-    t.equal(FloatRegStore.value, 123.123, 'should set value');
+    const Durability = new MuRDARegister(new MuFloat64());
+    const durabilityStore = Durability.createStore(100);
+    t.true(durabilityStore.apply(Durability, 99.97), 'decrease durability');
+    t.equal(durabilityStore.state(Durability, 100), 99.97, 'new durability');
 
-    const UserReg = new MuRDARegister(new MuStruct({
+    const User = new MuRDARegister(new MuStruct({
         id: new MuUint32(),
         name: new MuUTF8(),
     }));
-    const UserRegStore = UserReg.createStore({id: 0, name: ''});
+    const userStore = User.createStore(User.stateSchema.alloc());
     const u = {id: 12345, name: 'Mikola'};
-    t.true(UserRegStore.apply(UserReg, u), 'should always be true');
-    t.deepEqual(UserRegStore.value, u, 'should set value');
-    t.isNot(UserRegStore.value, u, 'value should be a copy');
+    t.true(userStore.apply(User, u), 'initiate user');
+    t.deepEqual(userStore.state(User, User.stateSchema.alloc()), u, 'user info');
+    t.isNot(userStore.state(User, User.stateSchema.alloc()), u, 'should be a copy');
     t.end();
 });
 
@@ -63,33 +54,27 @@ test('struct apply', (t) => {
 
     const actions:any[] = [];
     actions.push(dispatcher.rf(11.11));
-    actions.push(dispatcher.rs({f: 22.11, u: 'a'}));
-    actions.push(dispatcher.s.rf(33.11));
-    actions.push(dispatcher.s.rs({f: 44.11, u: 'b'}));
-    actions.push(dispatcher.s.s.rf(55.11));
-    actions.push(dispatcher.s.s.rs({f: 66.11, u: 'c'}));
-    actions.push(dispatcher.s.s.rs({f: 66.22, u: 'Iñtërnâtiônàlizætiøn☃💩'}));
-    actions.push(dispatcher.s.s.rf(55.22));
-    actions.push(dispatcher.s.rs({f: 44.22, u: 'Iñtërnâtiônàlizætiøn☃💩'}));
-    actions.push(dispatcher.s.rf(33.22));
-    actions.push(dispatcher.rs({f: 22.22, u: 'Iñtërnâtiônàlizætiøn☃💩'}));
-    actions.push(dispatcher.rf(11.22));
+    actions.push(dispatcher.rs({f: 22.22, u: 'a'}));
+    actions.push(dispatcher.s.rf(33.33));
+    actions.push(dispatcher.s.rs({f: 44.44, u: 'b'}));
+    actions.push(dispatcher.s.s.rf(55.55));
+    actions.push(dispatcher.s.s.rs({f: 66.66, u: 'Iñtërnâtiônàlizætiøn☃💩'}));
 
     for (let i = 0; i < actions.length; ++i) {
-       t.true(store.apply(S, actions[i]));
+       t.true(store.apply(S, actions[i]), JSON.stringify(actions[i]));
     }
     t.deepEqual(
         store.state(S, S.stateSchema.alloc()),
         {
             c: 0,
-            rf: 11.22,
-            rs: {f: 22.22, u: 'Iñtërnâtiônàlizætiøn☃💩'},
+            rf: 11.11,
+            rs: {f: 22.22, u: 'a'},
             s: {
-                rf: 33.22,
-                rs: {f: 44.22, u: 'Iñtërnâtiônàlizætiøn☃💩'},
+                rf: 33.33,
+                rs: {f: 44.44, u: 'b'},
                 s: {
-                    rf: 55.22,
-                    rs: {f: 66.22, u: 'Iñtërnâtiônàlizætiøn☃💩'},
+                    rf: 55.55,
+                    rs: {f: 66.66, u: 'Iñtërnâtiônàlizætiøn☃💩'},
                 },
             },
         },
@@ -102,28 +87,39 @@ test('map apply', (t) => {
     const M = new MuRDAMap(new MuUTF8(), new MuRDAConstant(new MuFloat64()));
     const store = M.createStore(M.stateSchema.identity);
     const dispatchers = M.action(store);
+    let action;
+
+    function checkState (expected) {
+        t.deepEqual(store.state(M, M.stateSchema.alloc()), expected, JSON.stringify(action));
+    }
 
     const e = Math.E;
     const pi = Math.PI;
     const log2e = Math.LOG2E;
     const log10e = Math.LOG10E;
 
-    t.true(store.apply(M, dispatchers.clear()), 'clean when empty');
-    t.false(store.apply(M, dispatchers.remove('')), `remove entry ''`);
-    t.false(store.apply(M, dispatchers.remove('--non-existent')), 'remove non-existent entry');
-    t.true(store.apply(M, dispatchers.set('--non-existent', 0)), 'set entry');
-    t.true(store.apply(M, dispatchers.set('--non-existent', 0)), 'set existent entry');
-    t.true(store.apply(M, dispatchers.remove('--non-existent')), 'remove existent entry');
-    t.true(store.apply(M, dispatchers.set('e', e)), 'set entry');
-    t.true(store.apply(M, dispatchers.set('pi', pi)), 'set another');
-    t.true(store.apply(M, {type: 'noop', data: 'whatever'}), 'noop');
-    t.deepEqual(store.state(M, {}), {e, pi});
-    t.true(store.apply(M, dispatchers.reset({log2e, log10e})), 'reset');
-    t.deepEqual(store.state(M, {}), {log2e, log10e});
-    t.true(store.apply(M, dispatchers.clear()), 'clear');
-    t.deepEqual(store.state(M, {}), {});
-    t.true(store.apply(M, dispatchers.set('Iñtërnâtiônàlizætiøn☃💩', 0)), 'key with emoji');
-    t.deepEqual(store.state(M, {}), {'Iñtërnâtiônàlizætiøn☃💩': 0});
+    t.true(store.apply(M, action = dispatchers.clear()), 'clear when empty');
+    checkState({});
+    t.true(store.apply(M, action = dispatchers.set('--non-existent', Infinity)), 'set entry');
+    checkState({'--non-existent': Infinity});
+    t.true(store.apply(M, action = dispatchers.set('--non-existent', -Infinity)), 'set existent entry');
+    checkState({'--non-existent': -Infinity});
+    t.true(store.apply(M, action = dispatchers.remove('--non-existent')), 'remove entry');
+    checkState({});
+    t.false(store.apply(M, action = dispatchers.remove('--non-existent')), 'remove non-existent entry');
+    checkState({});
+    t.true(store.apply(M, action = dispatchers.set('e', e)), 'set e');
+    checkState({e});
+    t.true(store.apply(M, action = dispatchers.set('pi', pi)), 'set pi');
+    checkState({e, pi});
+    t.true(store.apply(M, action = <any>{type: 'noop', data: 'whatever'}), 'noop');
+    checkState({e, pi});
+    t.true(store.apply(M, action = dispatchers.reset({log2e, log10e})), 'reset');
+    checkState({log2e, log10e});
+    t.true(store.apply(M, action = dispatchers.clear()), 'clear');
+    checkState({});
+    t.true(store.apply(M, action = dispatchers.set('Iñtërnâtiônàlizætiøn☃💩', 0)), 'key with emoji');
+    checkState({'Iñtërnâtiônàlizætiøn☃💩': 0});
     t.end();
 });
 
@@ -136,30 +132,35 @@ test('map of structs apply', (t) => {
     }));
     const store = M.createStore(M.stateSchema.identity);
     const dispatchers = M.action(store);
+    let action;
 
-    t.true(store.apply(M, dispatchers.set('first', {r: 11.11, s: {r: '11.22'}})), 'set entry');
-    t.true(store.apply(M, dispatchers.update('first').r(11.11)), 'same value');
-    t.true(store.apply(M, dispatchers.update('first').s.r('11.22')), 'same inner value');
-    t.true(store.apply(M, dispatchers.update('first').r(111.111)), 'different value');
-    t.true(store.apply(M, dispatchers.update('first').s.r('111.222')), 'different inner value');
-    t.true(store.apply(M, dispatchers.update('vanished').s.r('22.22')), 'should be noop');
-    t.true(store.apply(M, dispatchers.set('second', M.valueRDA.stateSchema.alloc())), 'set another');
-    t.false(store.apply(M, {type: 'update', data: {id: 'imaginary', action: {type: 'r', data: 0}}}), 'update non-existent entry');
-    t.deepEqual(store.state(M, {}), {
-        first: {r: 111.111, s: {r: '111.222'}},
-        second: {r: 0, s: {r: ''}},
-    });
+    function checkState (expected) {
+        t.deepEqual(store.state(M, M.stateSchema.alloc()), expected, JSON.stringify(action));
+    }
+
+    t.true(store.apply(M, action = dispatchers.set('first', {r: 11.11, s: {r: '11.22'}})), 'set entry');
+    checkState({first: {r: 11.11, s: {r: '11.22'}}});
+    t.true(store.apply(M, action = dispatchers.set('second', M.valueRDA.stateSchema.alloc())), 'set another');
+    checkState({first: {r: 11.11, s: {r: '11.22'}}, second: {r: 0, s: {r: ''}}});
+    t.true(store.apply(M, action = dispatchers.update('second').r(22.11)), 'set inner entry');
+    checkState({first: {r: 11.11, s: {r: '11.22'}}, second: {r: 22.11, s: {r: ''}}});
+    t.true(store.apply(M, action = dispatchers.update('second').s.r('22.22')), 'set innermost entry');
+    checkState({first: {r: 11.11, s: {r: '11.22'}}, second: {r: 22.11, s: {r: '22.22'}}});
+    t.true(store.apply(M, action = dispatchers.update('vanished').s.r('22.22')), 'update non-existent entry');
+    checkState({first: {r: 11.11, s: {r: '11.22'}}, second: {r: 22.11, s: {r: '22.22'}}});
+    t.false(store.apply(M, action = <any>{type: 'update', data: {id: 'imaginary', action: {type: 'r', data: 0}}}), 'update non-existent entry');
+    checkState({first: {r: 11.11, s: {r: '11.22'}}, second: {r: 22.11, s: {r: '22.22'}}});
 
     const defaultMap = {
         x: {r: 0, s: {r: ''}},
         y: {r: 0, s: {r: ''}},
         z: {r: 0, s: {r: ''}},
     };
-    t.true(store.apply(M, dispatchers.reset(defaultMap)), 'reset');
-    t.deepEqual(store.state(M, {}), defaultMap);
+    t.true(store.apply(M, action = dispatchers.reset(defaultMap)), 'reset');
+    checkState(defaultMap);
 
-    t.true(store.apply(M, dispatchers.clear()), 'clear');
-    t.deepEqual(store.state(M, {}), {});
+    t.true(store.apply(M, action = dispatchers.clear()), 'clear');
+    checkState({});
     t.end();
 });
 
@@ -170,28 +171,37 @@ test('map of maps apply', (t) => {
     );
     const store = M.createStore(M.stateSchema.identity);
     const dispatchers = M.action(store);
+    let action;
+
+    function checkState (expected) {
+        t.deepEqual(store.state(M, M.stateSchema.alloc()), expected, JSON.stringify(action));
+    }
 
     const e = Math.E;
     const pi = Math.PI;
     const log2e = Math.LOG2E;
     const log10e = Math.LOG10E;
 
-    t.true(store.apply(M, dispatchers.set('constants', {})), 'set outer entry');
-    t.true(store.apply(M, dispatchers.update('constants').clear()), 'clear when empty');
-    t.false(store.apply(M, dispatchers.update('constants').remove('')), `remove entry ''`);
-    t.false(store.apply(M, dispatchers.update('constants').remove('--non-existent')), 'remove non-existent entry');
-    t.true(store.apply(M, dispatchers.update('constants').set('--non-existent', 0)), 'set entry');
-    t.true(store.apply(M, dispatchers.update('constants').set('--non-existent', 0)), 'set existent entry');
-    t.true(store.apply(M, dispatchers.update('constants').remove('--non-existent')), 'remove existent entry');
-    t.true(store.apply(M, dispatchers.update('constants').set('e', e)), 'set entry');
-    t.true(store.apply(M, dispatchers.update('constants').set('pi', pi)), 'set another');
-    t.deepEqual(store.state(M, {}), {constants: {e, pi}});
-    t.true(store.apply(M, dispatchers.update('constants').reset({log2e, log10e})), 'reset');
-    t.deepEqual(store.state(M, {}), {constants: {log2e, log10e}}, 'after reset');
-    t.true(store.apply(M, dispatchers.update('constants').clear()), 'clear');
-    t.deepEqual(store.state(M, {}), {constants: {}});
-    t.true(store.apply(M, dispatchers.clear()), 'outer clear');
-    t.deepEqual(store.state(M, {}), {});
+    t.true(store.apply(M, action = dispatchers.set('constants', {})), 'set outer entry');
+    checkState({constants: {}});
+    t.true(store.apply(M, action = dispatchers.update('constants').clear()), 'clear when empty');
+    checkState({constants: {}});
+    t.true(store.apply(M, action = dispatchers.update('constants').set('--non-existent', Infinity)), 'set entry');
+    checkState({constants: {'--non-existent': Infinity}});
+    t.true(store.apply(M, action = dispatchers.update('constants').set('--non-existent', -Infinity)), 'set entry again');
+    checkState({constants: {'--non-existent': -Infinity}});
+    t.true(store.apply(M, action = dispatchers.update('constants').remove('--non-existent')), 'remove entry');
+    checkState({constants: {}});
+    t.false(store.apply(M, action = dispatchers.update('constants').remove('--non-existent')), 'remove non-existent entry');
+    checkState({constants: {}});
+    t.true(store.apply(M, action = dispatchers.update('constants').set('e', e)), 'set e');
+    checkState({constants: {e}});
+    t.true(store.apply(M, action = dispatchers.update('constants').set('pi', pi)), 'set pi');
+    checkState({constants: {e, pi}});
+    t.true(store.apply(M, action = dispatchers.update('constants').reset({log2e, log10e})), 'reset');
+    checkState({constants: {log2e, log10e}});
+    t.true(store.apply(M, action = dispatchers.update('constants').clear()), 'clear');
+    checkState({constants: {}});
     t.end();
 });
 
@@ -204,24 +214,24 @@ test('map of structs of maps of structs apply', (t) => {
     }));
     const store = M.createStore(M.stateSchema.identity);
     const dispatchers = M.action(store);
+    let action;
 
-    t.true(store.apply(M, dispatchers.set('outer', M.valueRDA.stateSchema.alloc())));
-    t.true(store.apply(M, dispatchers.update('outer').m.clear()), 'clear when empty');
-    t.false(store.apply(M, dispatchers.update('outer').m.remove('')), `remove entry ''`);
-    t.false(store.apply(M, dispatchers.update('outer').m.remove('--non-existent')), 'remove non-existent entry');
-    t.true(store.apply(M, dispatchers.update('outer').m.set('--non-existent', M.valueRDA.rdas.m.valueRDA.stateSchema.alloc())), 'set entry');
-    t.true(store.apply(M, dispatchers.update('outer').m.set('--non-existent', M.valueRDA.rdas.m.valueRDA.stateSchema.alloc())), 'set entry again');
-    t.true(store.apply(M, dispatchers.update('outer').m.remove('--non-existent')), 'remove existent entry');
-    t.true(store.apply(M, dispatchers.update('outer').m.set('inner', M.valueRDA.rdas.m.valueRDA.stateSchema.alloc())));
-    t.true(store.apply(M, dispatchers.update('outer').m.update('inner').rf(111.111)), 'update inner prop');
-    t.true(store.apply(M, dispatchers.update('outer').m.update('inner').ru('111.222')), 'update another inner prop');
-    t.deepEqual(store.state(M, {}), {outer: {m: {inner: {rf: 111.111, ru: '111.222'}}}});
-    t.true(store.apply(M, dispatchers.update('outer').m.reset({inner: {rf: 222.111, ru: '222.222'}})), 'reset');
-    t.deepEqual(store.state(M, {}), {outer: {m: {inner: {rf: 222.111, ru: '222.222'}}}});
-    t.true(store.apply(M, dispatchers.update('outer').m.clear()), 'clear');
-    t.deepEqual(store.state(M, {}), {outer: {m: {}}});
-    t.true(store.apply(M, dispatchers.clear()), 'outer clear');
-    t.deepEqual(store.state(M, {}), {});
+    function checkState (expected) {
+        t.deepEqual(store.state(M, M.stateSchema.alloc()), expected, JSON.stringify(action));
+    }
+
+    t.true(store.apply(M, action = dispatchers.set('outer', M.valueRDA.stateSchema.alloc())));
+    checkState({outer: {m: {}}});
+    t.true(store.apply(M, action = dispatchers.update('outer').m.set('inner', M.valueRDA.rdas.m.valueRDA.stateSchema.alloc())));
+    checkState({outer: {m: {inner: {rf: 0, ru: ''}}}});
+    t.true(store.apply(M, action = dispatchers.update('outer').m.update('inner').rf(111.111)), 'update inner prop');
+    checkState({outer: {m: {inner: {rf: 111.111, ru: ''}}}});
+    t.true(store.apply(M, action = dispatchers.update('outer').m.update('inner').ru('111.222')), 'update another inner prop');
+    checkState({outer: {m: {inner: {rf: 111.111, ru: '111.222'}}}});
+    t.true(store.apply(M, action = dispatchers.update('outer').m.reset({inner: {rf: 222.111, ru: '222.222'}})), 'reset');
+    checkState({outer: {m: {inner: {rf: 222.111, ru: '222.222'}}}});
+    t.true(store.apply(M, action = dispatchers.update('outer').m.clear()), 'clear');
+    checkState({outer: {m: {}}});
     t.end();
 });
 
