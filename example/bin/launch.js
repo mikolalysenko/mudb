6020c3317fd80723840631c584f8725ea3436791
He Diyi <esdoppio@gmail.com>
2019-04-01 10:04:24 +0800

    rewrite trackpad example with MuDelta

--- example/bin/launch.js
+++ example/bin/launch.js
@@ -11,7 +11,7 @@ if (!exp) {
 const mudoDir = path.join(path.resolve('..'), 'tool/mudo')
 const expDir = path.join(path.resolve('.'), exp)
 const sockType = argv[1] || 'web'
-const cmd = `${mudoDir}/bin/mudo.js --client ${expDir}/client.js --server ${expDir}/server.js --socket ${sockType} --open`
+const cmd = `node ${mudoDir}/bin/mudo.js --client ${expDir}/client.js --server ${expDir}/server.js --socket ${sockType} --open`
 
 exec(cmd, (err, stdout) => {
     if (err) {
--- example/trackpad/client.ts
+++ example/trackpad/client.ts
@@ -1,57 +1,58 @@
-import { GameSchema } from './schema';
-import { MuClient } from 'mudb/client';
-import { MuClientState } from 'mudb/state/client';
+import { MuClient } from '../../client';
+import { MuDeltaClient } from '../../delta/client';
+import { PlayerSetSchema, ControllerSchema } from './schema';
 
 export = function (client:MuClient) {
     const canvas = document.createElement('canvas');
-    canvas.style.padding = '0px';
-    canvas.style.margin = '0px';
-    document.body.appendChild(canvas);
     const context = canvas.getContext('2d');
     if (!context) {
         document.body.innerText = 'canvas not supported';
         return;
     }
 
-    const protocol = new MuClientState({
-        schema: GameSchema,
-        client,
-        windowSize: 0,
-    });
+    canvas.style.padding = '0px';
+    canvas.style.margin = '0px';
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    document.body.appendChild(canvas);
 
-    protocol.configure({
-        ready: () => {
-            draw();
-            canvas.addEventListener('mousemove', (ev) => {
-                protocol.state.x = ev.clientX / canvas.width;
-                protocol.state.y = ev.clientY / canvas.height;
-                protocol.commit();
-            });
-            protocol.state.color = `#${Math.floor(Math.random() * 0xefffff + 0x100000).toString(16)}`;
-            protocol.commit();
-        },
-    });
-
-    function draw () {
-        if (!context || !client.running) {
+    function draw (players) {
+        if (!context) {
             return;
         }
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
 
         context.fillStyle = '#fff';
         context.fillRect(0, 0, canvas.width, canvas.height);
 
-        Object.keys(protocol.server.state).forEach((id) => {
-            const {x, y, color} = protocol.server.state[id];
+        Object.keys(players).forEach((id) => {
+            const { color, x, y } = players[id];
             context.fillStyle = color;
             context.fillRect(x * canvas.width - 10, y * canvas.height - 10, 10, 10);
         });
-
-        context.fillStyle = protocol.state.color;
-        context.fillRect(protocol.state.x * canvas.width - 15, protocol.state.y * canvas.height - 15, 15, 15);
-        requestAnimationFrame(draw);
     }
+
+    const deltaClient = new MuDeltaClient({
+        client: client,
+        schema: PlayerSetSchema,
+    });
+    deltaClient.configure({
+        change: (state) => {
+            requestAnimationFrame(() => draw(state));
+        },
+    });
+
+    const playProtocol = client.protocol(ControllerSchema);
+    playProtocol.configure({
+        ready: () => {
+            canvas.addEventListener('mousemove', (ev) => {
+                playProtocol.server.message.move({
+                    x: ev.clientX / canvas.width,
+                    y: ev.clientY / canvas.height,
+                });
+            });
+        },
+        message: () => { },
+    });
 
     client.start();
 };
