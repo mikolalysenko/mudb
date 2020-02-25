46bc0669dbe4c4509820f2a48b0d72869aba63db
He Diyi <esdoppio@gmail.com>
2020-02-25 16:43:57 +0800

    bench array and dictionary against identity base

--- src/schema/bench/array.ts
+++ src/schema/bench/array.ts
@@ -1,26 +1,27 @@
 import { MuArray, MuASCII } from '../';
 import { deltaByteLength, diffPatchDuration } from './_do';
 
-const uint8Array = new MuArray(new MuASCII(), Infinity);
+const array = new MuArray(new MuASCII(), Infinity);
 
-deltaByteLength(uint8Array, [], ['a']);
-deltaByteLength(uint8Array, [], ['a', 'b']);
-deltaByteLength(uint8Array, [], ['a', 'b', 'c']);
-deltaByteLength(uint8Array, ['a'], ['a', 'b']);
-deltaByteLength(uint8Array, ['b'], ['a', 'b']);
+deltaByteLength(array, [], ['a']);
+deltaByteLength(array, [], ['a', 'b']);
+deltaByteLength(array, [], ['a', 'b', 'c']);
+deltaByteLength(array, ['a'], ['a', 'b']);
+deltaByteLength(array, ['b'], ['a', 'b']);
 
-deltaByteLength(uint8Array, ['a'], []);
-deltaByteLength(uint8Array, ['a', 'b'], []);
-deltaByteLength(uint8Array, ['a', 'b'], ['a']);
-deltaByteLength(uint8Array, ['a', 'b', 'c'], ['a']);
+deltaByteLength(array, ['a'], []);
+deltaByteLength(array, ['a', 'b'], []);
+deltaByteLength(array, ['a', 'b'], ['a']);
+deltaByteLength(array, ['a', 'b', 'c'], ['a']);
 
-deltaByteLength(uint8Array, ['a'], ['b']);
-deltaByteLength(uint8Array, ['a', 'b'], ['b', 'c']);
+deltaByteLength(array, ['a'], ['b']);
+deltaByteLength(array, ['a', 'b'], ['b', 'c']);
 
-const a1 = ['a', 'a', 'a', 'a', 'a'];
-const a2 = ['b', 'c', 'd', 'e', 'f'];
+const a0 = [];
+const a1 = ['a', 'b', 'c', 'd', 'e'];
 
-diffPatchDuration(uint8Array, a1, a1, 1e3);
+diffPatchDuration(array, a1, a1, 1e3);
 
-diffPatchDuration(uint8Array, a1, a2, 1e3);
-diffPatchDuration(uint8Array, a1, a2, 1e5);
+diffPatchDuration(array, a0, a1, 1e3);
+diffPatchDuration(array, a0, a1, 1e4);
+diffPatchDuration(array, a0, a1, 1e5);
