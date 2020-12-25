b71351b38bc6b7e6fe86fe73010d42f9d40bd476
mikola <mikolalysenko@gmail.com>
2020-12-25 09:06:27 +0800

    fix mudb stringify

--- src/util/stringify.ts
+++ src/util/stringify.ts
@@ -1,43 +1,73 @@
-const seen:object[] = [];
-
-export function stableStringify (x_) {
-    const x = x_ && x_.toJSON && typeof x_.toJSON === 'function' ? x_.toJSON() : x_;
-    if (x === undefined) { return ''; }
-    if (x === true) { return 'true'; }
-    if (x === false) { return 'false'; }
-    if (typeof x === 'number') { return isFinite(x) ? '' + x : 'null'; }
-    if (typeof x !== 'object') { return JSON.stringify(x); }
-
-    if (x === null) { return 'null'; }
-    if (Array.isArray(x)) {
-        let str = '[';
-        const tail = x.length - 1;
-        for (let i = 0; i < tail; ++i) {
-            str += (stableStringify(x[i]) || 'null') + ',';
+export function stableStringify (base:any) {
+    console.log('stable stringify:', base);
+    const result:string[] = [];
+    const seen = new Set<object>();
+    function stringify (x_:any) : boolean {
+        const x = x_ && x_.toJSON && typeof x_.toJSON === 'function' ? x_.toJSON() : x_;
+        if (x === undefined) {
+            return false;
         }
-        if (tail >= 0) {
-            str += stableStringify(x[tail]) || 'null';
+        // handle base cases
+        if (typeof x === 'boolean') {
+            result.push('' + x)
+            return true;
         }
-        return str + ']';
-    } else {
-        if (seen.indexOf(x) !== -1) {
-            throw new TypeError('Converting circular structure to JSON');
+        if (typeof x === 'number') {
+            result.push(isFinite(x) ? '' + x : 'null');
+            return true;
         }
-        const idx = seen.push(x) - 1;
-
-        let str = '';
-        const keys = Object.keys(x).sort();
-        for (let i = 0; i < keys.length; ++i) {
-            const key = keys[i];
-            const val = stableStringify(x[key]);
-            if (val !== undefined) {
-                if (str) { str += ','; }
-                str += `${JSON.stringify(key)}:${val}`;
+        if (typeof x !== 'object') {
+            const res = JSON.stringify(x);
+            if (typeof res === 'undefined') {
+                return false;
             }
+            result.push(res);
+            return true;
+        }
+        if (x === null) {
+            result.push('null');
+            return true;
         }
 
-        seen[idx] = seen[seen.length - 1];
-        seen.pop();
-        return `{${str}}`;
+        // hard cases
+        if (Array.isArray(x)) {
+            result.push('[');
+            for (let i = 0; i < x.length; ++i) {
+                if (!stringify(x[i])) {
+                    result.push('null');
+                }
+                if (i < x.length - 1) {
+                    result.push(',');
+                }
+            }
+            result.push(']');
+            return true;
+        } else {
+            if (seen.has(x)) {
+                throw new TypeError('Converting circular structure to JSON');
+            }
+            seen.add(x);
+            result.push('{');
+
+            const keys = Object.keys(x).sort();
+            let needsComma = false;
+            for (let i = 0; i < keys.length; ++i) {
+                const key = keys[i];
+                if (needsComma) {
+                    result.push(',');
+                }
+                result.push(`${JSON.stringify(key)}:`);
+                if (!stringify(x[key])) {
+                    result.pop();
+                } else {
+                    needsComma = true;
+                }
+            }
+
+            seen.delete(x);
+            result.push('}');
+            return true;
+        }
     }
-};
+    return stringify(base);
+}
